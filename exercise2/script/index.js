c6efd22d35ff24b97adf3c3b9c667e98ff9e9f37
Elena <elenamam1504@gmail.com>
2023-10-16 20:30:52 -0700

    first commit

---
+++ exercise2/script/index.js
@@ -0,0 +1,10 @@
+function showMessage() {
+  //код функции
+}
+let text = 'Я учу JavaScript!';
+
+function showMessage(text) {
+  return 'Я учу JavaScript!';
+}
+
+console.log(showMessage(text));
---
+++ exercise3/script/index.js
@@ -0,0 +1,11 @@
+let image = document.getElementById('elem-img');
+
+function showimageFirst() {
+  image.src = 'assets/img/sticker_1.png';
+}
+console.log(showimageFirst());
+
+function showimageSecond() {
+  image.src = 'assets/img/sticker_6.png';
+}
+console.log(showimageSecond());
